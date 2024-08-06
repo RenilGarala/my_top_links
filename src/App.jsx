44012d82c4bd845dfd1fd2e79cbb2748c10134b5
renil <renilgarala360004@gmail.com>
2024-08-06 12:00:03 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-gradient-to-r from-blue-950 to-black w-screen h-screen flex justify-center items-center'>
+      <div className='bg-gradient-to-r from-blue-950 to-black sm:w-screen w-screen h-screen flex justify-center items-center'>
         <Home/>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      // => @media (max-width: 1536px) { ... }
+      dxl: {max: "1536px" },
+  
+      // => @media (max-width: 1279px) { ... }
+      xl: { max: "1279px" },
+  
+      // => @media (max-width: 1023px) { ... }
+      lg: { max: "1023px" },
+  
+      // => @media (max-width: 767px) { ... }
+      md: { max: "767px" },
+  
+      // => @media (max-width: 639px) { ... }
+      sm: { max: "639px" },
+    },
   },
+  
   plugins: [],
+  darkMode: "class",
 }
 
